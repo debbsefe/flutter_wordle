5b1b99be72ae18a3d1a70549ea4144dead7e1463
Mamus Eferha <eferhamamus@gmail.com>
2022-01-24 13:41:28 +0100

    fix failing test

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
         child: Scaffold(
       body: Center(
         child: Text('Wordle!'),
