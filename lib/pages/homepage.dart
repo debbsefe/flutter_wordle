824385b6b5b6e3b5f908403fc066344bcccd686b
Mamus Eferha <eferhamamus@gmail.com>
2022-01-24 22:17:43 +0100

    feat: create gridview ui

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wordle/widgets/grid_view_widget.dart';
 import 'package:wordle/widgets/header_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,10 +9,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        children: const [
-          HeaderWidget(),
-        ],
+      body: Container(
+        margin: const EdgeInsets.only(top: 30.0),
+        child: Center(
+          child: SizedBox(
+            width: 600,
+            child: Column(
+              children: const [
+                HeaderWidget(),
+                SizedBox(height: 50),
+                GridViewWidget(),
+              ],
+            ),
+          ),
+        ),
       ),
     ));
   }
